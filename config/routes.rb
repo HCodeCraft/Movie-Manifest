1701d64d34847dab534976ea2a4d13bac9f01a86
Hanna <marmalademarmoset@gmail.com>
2023-07-11 11:39:29 -0700

    Add movie_list_id to movies table

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :movie_lists, only: [:index, :create]
-  resources :movies, only: [:index, :create]
-  
+  resources :movie_lists, only: [:index, :create] do
+    resources :movies
+  end
+
+  resources :movies
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
