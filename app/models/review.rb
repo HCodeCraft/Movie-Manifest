de34e8a9584834c9bc6fb30e70e4090cc24dcfe5
Hanna <marmalademarmoset@gmail.com>
2023-07-13 17:51:46 -0700

    Filling out validations for movie, review and user models

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,8 @@ class Review < ApplicationRecord
    belongs_to :user
    belongs_to :movie
 
+   validates :review, length: { minimum: 25 }
+   validates :rating, length: { in: 0..5}
+
 
 end
--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,4 +1,7 @@
 class ReviewSerializer < ActiveModel::Serializer
-  attributes :review, :watched, :rating
+  attributes :review, :watched, :rating 
   has_many :movies
+  has_one :user
+#   ^ Means that the review has a single associated user that will be included
+# when the Review is serialized
 end
