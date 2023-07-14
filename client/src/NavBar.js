7951252bfabdb0ddcc2549171ebd396c52f23c75
Hanna <marmalademarmoset@gmail.com>
2023-07-13 21:12:39 -0700

    Add links and link locations to NavBar component

--- client/src/NavBar.js
+++ client/src/NavBar.js
@@ -1,13 +1,24 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
+
+    // I need to import the user id in here for linking to
+    // "My Movies/Reviews" and to display the User's username
   return (
     <>
-    <div><h1>Movie Manifest</h1></div>
-    <div><Link>Home</Link> <Link>My Movies/Reviews</Link> <Link>All Movies</Link> <Link>Add a Movie</Link></div>
+      <div>
+        <h1>Movie Manifest</h1>
+      </div>
+      <div>
+        <Link to={"/"}>Home</Link>{" "}
+        <Link to={`/users/#{id}/movies`}>My Movies/Reviews</Link>{" "}
+        <Link to={"/movies"}>All Movies</Link>{" "}
+        <Link to={"movies/new"}>Add a Movie</Link>
+        <p>Logout</p>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
