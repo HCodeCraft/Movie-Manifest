3721837b3997ee066f307bfe3f7d0f2a65807284
Hanna <marmalademarmoset@gmail.com>
2023-07-14 12:32:24 -0700

    Add user and session routes to routes.rb, rename NewMovieForm to NewMovie, and NewUserForm to Signup

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,28 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
+import { UserProvider } from "./context/user"
 import Home from "./Home";
 import NavBar from "./NavBar";
-import NewUserForm from "./NewUserForm";
+import Signup from "./Signup";
 import Movies from "./Movies"
 import UserMovies from "./UserMovies"
 import Movie from "./Movie";
-import NewMovieForm from "./NewMovieForm";
+import NewMovie from "./NewMovie";
 
 function App() {
   return (
     <>
+    <UserProvider>
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/movies" element={<Movies/>} />
         <Route path="/movies/:id" element={<Movie />} />
-        <Route path="/movies/new" element={<NewMovieForm />} />
-        <Route path="/users/new" element={<NewUserForm/>} />
+        <Route path="/movies/new" element={<NewMovie/>} />
+        <Route path="/users/new" element={<Signup/>} />
         <Route path="/users/:id/movies" element={<UserMovies/>} />
       </Routes>
+      </UserProvider>
     </>
   );
 }
