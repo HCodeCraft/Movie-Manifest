feaaffdfd74c8faf5ca721496da432c9304b4b6b
Hanna <marmalademarmoset@gmail.com>
2023-07-14 19:17:57 -0700

    Add create action to the SessionsController

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,10 +1,18 @@
-import React, { useContext } from 'react'
-import { useNavigate, Link } from "react-router-dom"
+import React, { useContext, useState } from 'react'
+import { useNavigate, NavLink } from "react-router-dom"
 import { UserContext } from './context/user'
 
 function Home() {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [error, setError] = useState('')
   const { user, login } = useContext(UserContext)
     const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+      e.preventDefault()
+
+    }
 
     // I want the Login form to dissapear if user is logged in, and
     // I want it to say Welcome <User>! 
@@ -16,11 +24,11 @@ function Home() {
                <h3> Never forget a movie again!</h3>
     
           <h2>Please Login: </h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>Username:</label>
-                <input name="username" type="text" ></input>
+                <input name="username" type="text" value={username} onChange={(e) => setUsername(e.target.value)} ></input>
                 <label>Password:</label>
-                <input name="username" type="text" ></input>
+                <input name="username" type="password" value={password} onChange={(e) => setPassword(e.target.value)}></input>
                 <input type="submit" />
             </form>
             <h3>Or:</h3>
@@ -34,9 +42,9 @@ function Home() {
         <div>
           <h3>Welcome {user.username}!</h3>
           Feel free to:
-          <Link to={'/movies'}><button>Browse Movies</button></Link>
+          <NavLink to={'/movies'}><button>Browse Movies</button></NavLink>
           Or
-          <Link to={'/movies/new'}><button>Add a Movie</button></Link>
+          <NavLink to={'/movies/new'}><button>Add a Movie</button></NavLink>
         </div>
       )
     }
--- client/src/NavBar.js
+++ client/src/NavBar.js
@@ -1,24 +1,41 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useContext } from "react";
+import { NavLink } from "react-router-dom";
+import { UserContext } from "./context/user";
 
 const NavBar = () => {
+  const { user, logout } = useContext(UserContext);
 
-    // I need to import the user id in here for linking to
-    // "My Movies/Reviews" and to display the User's username
-  return (
-    <>
+  const logoutUser = () => {
+    fetch('/logout')
+    .then(() => {
+      logout()
+    })
+  }
+
+  // I need to import the user id in here for linking to
+  // "My Movies/Reviews" and to display the User's username
+  if (user) {
+    return (
       <div>
-        <h1>Movie Manifest</h1>
+        <NavLink to={"/"}>Home</NavLink>{" "}
+        <NavLink to={`/users/#{id}/movies`}>My Movies/Reviews</NavLink>{" "}
+        <NavLink to={"/movies"}>All Movies</NavLink>{" "}
+        <NavLink to={"movies/new"}>Add a Movie</NavLink>
+        <p>{user ? user.username : ""}</p>
+        <button onClick={logoutUser}>Logout</button>
+        <br />
       </div>
-      <div>
-        <Link to={"/"}>Home</Link>{" "}
-        <Link to={`/users/#{id}/movies`}>My Movies/Reviews</Link>{" "}
-        <Link to={"/movies"}>All Movies</Link>{" "}
-        <Link to={"movies/new"}>Add a Movie</Link>
-        <p>Logout</p>
-      </div>
-    </>
-  );
+    );
+  } else {
+    return (
+      <>
+        <div>
+          <h1>Movie Manifest</h1>
+          {/* <NavLink to={'/signup'}>Signup</NavLink> */}
+        </div>
+      </>
+    );
+  }
 };
 
 export default NavBar;
