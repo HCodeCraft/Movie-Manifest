8424307235e88d5b3aef2ea4ddef54559e4f851c
Hanna <marmalademarmoset@gmail.com>
2023-07-14 17:33:11 -0700

    Add code to Signup component(form and state etc.) as well as add the create action code to the users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,13 @@ class UsersController < ApplicationController
 
     #create
     def create
+        user = User.create(user_params)
+        if user.valid?
+            session[:user_id] = user.id
+            render json: user
+        else
+            render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+        end
 
     end
 
@@ -11,5 +18,11 @@ class UsersController < ApplicationController
             render json: user
         else  
             render json: { error: "Not Authorized"}, status: :unauthorized
+    end
+
+    private
+
+    def user_params
+        params.permit(:username, :password, :password_confirmation)
     end
 end
